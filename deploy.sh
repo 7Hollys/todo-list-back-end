1661bc41994f68052c41e8f48fa3002beaa39deb
inhwa <inhwa@allocation.co.kr>
2020-07-06 18:11:30 +0900

    배포 테스트

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker rm -f todo
-rm -rf  /mys3bucket/todoList-0.0.1-SNAPSHOT.jar
 rm -rf  /mys3bucket/code-deploy-todo-list-back-end
 unzip -o /mys3bucket/todo.zip -d /mys3bucket
 cp /mys3bucket/code-deploy-todo-list-back-end/todoList-0.0.1-SNAPSHOT.jar /home/ubuntu/todo/
-docker run --restart always -v /home/ubuntu/todo/todoList-0.0.1-SNAPSHOT.jar:/root/todoList-0.0.1-SNAPSHOT.jar -d --name todo -p 80:8080 openjdk:8 java -jar /root/todoList-0.0.1-SNAPSHOT.jar
+docker service update --force 7hollys_todo
