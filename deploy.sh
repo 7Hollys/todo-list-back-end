f1ede2e7ec0ce66ad59f3ab69af9ed86650f2cc9
inhwa <inhwa@allocation.co.kr>
2020-07-06 14:21:14 +0900

    설정파일 업데이트

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker rm -f todo
-rm -rf  /mys3bucket/jar/todoList-0.0.1-SNAPSHOT.jar
-rm -rf  /mys3bucket/jar/code-deploy-todo-list-back-end
-unzip -o /mys3bucket/jar/todo.zip -d /mys3bucket/jar
-cp /mys3bucket/jar/code-deploy-todo-list-back-end/todoList-0.0.1-SNAPSHOT.jar /home/ubuntu/todo/
+rm -rf  /mys3bucket/todoList-0.0.1-SNAPSHOT.jar
+rm -rf  /mys3bucket/code-deploy-todo-list-back-end
+unzip -o /mys3bucket/todo.zip -d /mys3bucket/jar
+cp /mys3bucket/code-deploy-todo-list-back-end/todoList-0.0.1-SNAPSHOT.jar /home/ubuntu/todo/
 docker run --restart always -v /home/ubuntu/todo/todoList-0.0.1-SNAPSHOT.jar:/root/todoList-0.0.1-SNAPSHOT.jar -d --name todo -p 80:8080 openjdk:8 java -jar /root/todoList-0.0.1-SNAPSHOT.jar
