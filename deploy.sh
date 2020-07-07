55d6f71f5400896310734b05a46514bc8c06a9a9
inhwa <inhwa@allocation.co.kr>
2020-07-07 10:20:58 +0900

    ec2 unzip 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -rf  /mys3bucket/code-deploy-todo-list-back-end
-unzip -o /mys3bucket/todo.zip -d /mys3bucket
-cp /mys3bucket/code-deploy-todo-list-back-end/todoList-0.0.1-SNAPSHOT.jar /home/ubuntu/todo/
+rm -rf  /home/ubuntu/todo/code-deploy-todo-list-back-end
+cp /mys3bucket/todo.zip /home/ubuntu/todo/
+unzip -o /home/ubuntu/todo/todo.zip -d /home/ubuntu/todo/
+mv /home/ubuntu/todo/code-deploy-todo-list-back-end/todoList-0.0.1-SNAPSHOT.jar /home/ubuntu/todo/
 docker service update --force 7hollys_todo
