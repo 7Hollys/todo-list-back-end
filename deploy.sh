3cb66eee84cf2f03bfed8c4660b2e9778a0799a2
inhwa <47582614+inhwa-son@users.noreply.github.com>
2020-07-19 14:22:01 +0900

    Update deploy.sh
    docker service name

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ rm -rf  /home/ubuntu/todo/code-deploy-todo-list-back-end
 cp /mys3bucket/todo.zip /home/ubuntu/todo/
 unzip -o /home/ubuntu/todo/todo.zip -d /home/ubuntu/todo/
 mv /home/ubuntu/todo/code-deploy-todo-list-back-end/todoList-0.0.1-SNAPSHOT.jar /home/ubuntu/todo/
-docker service update --force 7hollys_todo
+docker service update --force todo_backend
